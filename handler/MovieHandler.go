21005b1c822f0ef1a44467dcff574199ea96bcf0
kishansarsecha <kishans@thoughtworks.com>
2021-07-08 13:05:45 +0530

    [7] Add handler to get a movie by name

--- handler/MovieHandler.go
+++ handler/MovieHandler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/ksarsecha/movie_rental/domain"
 	"github.com/ksarsecha/movie_rental/http/utils"
 	"net/http"
@@ -28,4 +29,21 @@ func CreateMovieHandler(response http.ResponseWriter, request *http.Request) {
 
 	size, _ := response.Write(movieBytes)
 	response.Header().Set("content-Length", strconv.Itoa(size))
+}
+
+func GetMovieHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	movieName := vars["name"]
+
+	for _, movie := range movies {
+		if movie.Name == movieName {
+			response.Header().Set("Content-Type", "application/json")
+			response.WriteHeader(http.StatusOK)
+			movieBytes, _ := json.Marshal(movie)
+
+			size, _ := response.Write(movieBytes)
+			response.Header().Set("content-Length", strconv.Itoa(size))
+			break
+		}
+	}
 }
--- handler/MovieHandler_test.go
+++ handler/MovieHandler_test.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"bytes"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/ksarsecha/movie_rental/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"net/http"
@@ -15,12 +18,12 @@ func TestShouldCreateAMovie(t *testing.T) {
 	request, err := http.NewRequest(http.MethodPost, "/movie", bytes.NewBufferString(movie))
 	require.NoError(t, err)
 
-	response := httptest.NewRecorder()
+	responseRecorder := httptest.NewRecorder()
 
-	CreateMovieHandler(response, request)
+	CreateMovieHandler(responseRecorder, request)
 
-	assert.Equalf(t, http.StatusCreated, response.Code,
-		"Expected 201 Created, received %v. Reason: %s", response.Code, response.Body.String())
+	assert.Equalf(t, http.StatusCreated, responseRecorder.Code,
+		"Expected 201 Created, received %v. Reason: %s", responseRecorder.Code, responseRecorder.Body.String())
 	assert.Equal(t, 1, len(movies))
 }
 
@@ -34,4 +37,30 @@ func TestShouldReturnBadRequestForInvalidRequestBody(t *testing.T) {
 
 	assert.Equalf(t, http.StatusBadRequest, response.Code,
 		"Expected 400 Bad request, received %v", response.Code)
+}
+
+func TestShouldReturnMovie(t *testing.T) {
+	movies = append(movies, domain.Movie{
+		Name:        "TestName",
+		Description: "TestDescription",
+	})
+
+	request, err := http.NewRequest(http.MethodGet, "/movie/TestName", nil)
+	require.NoError(t, err)
+
+	request = mux.SetURLVars(request, map[string]string{"name": "TestName"})
+	responseRecorder := httptest.NewRecorder()
+
+	GetMovieHandler(responseRecorder, request)
+
+	assert.Equalf(t, http.StatusOK, responseRecorder.Code,
+		"Expected 200 OK, received %v. Reason %s", responseRecorder.Code, responseRecorder.Body.String())
+
+	var movie domain.Movie
+	decoder := json.NewDecoder(responseRecorder.Body)
+	err = decoder.Decode(&movie)
+	require.NoError(t, err)
+
+	assert.Equal(t, "TestName", movie.Name)
+	assert.Equal(t, "TestDescription", movie.Description)
 }
